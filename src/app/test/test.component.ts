841d18e0f45d4b94fdfdb018c3b7fdeb7cb64aa7
Prince Mfinanga <noahprince2@icloud.com>
2024-08-21 12:14:55 +0300

    binding and operations

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,25 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-    <h2>
-      Welcome {{name}}!
-    </h2>
-    <button (click)="onClick($event)">Greet</button>
-    <button (click)="greeting='Welcome Prince'">Greet</button>
-    {{greeting}}
-
+    <div *ngFor="let color of colors; even as e">
+      <h2>{{e}}) {{color}}</h2>
+    </div>
   `,
-  styles: [``]
+  styles: []
 })
 export class TestComponent implements OnInit {
-  public name = "Prince Mfinanga";
-  public greeting = "";
+
+  public colors = ["red","blue","green","yellow"];
   constructor() {}
   ngOnInit() {
   }
 
-  onClick(event: any) {
-    console.log(event);
-    this.greeting = event.type;
-  }
+  
 }
